fc346f25e83c5c02bb715dabaa710830d050c3be
Alexey Frunza <tvoih8r@mail.ru>
2021-04-14 22:25:11 +0300

    got key handle

--- index.js
+++ index.js
@@ -1,16 +1,72 @@
+
+
 const audioClips = [
     {
         keyCode:81,
         keyTrigger: 'Q',
         id: 'Chord-1',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        url: './sounds/chord-1.mp3'
     }
+    ,
+    {
+        keyCode: 87,
+        keyTrigger: 'W',
+        id: 'Chord-2',
+        url: './sounds/chord-2.mp3'
+    }
+    ,
+    {
+        keyCode: 69,
+        keyTrigger: 'E',
+        id: 'Chord-3',
+        url: './sounds/chord-3.mp3'
+    }
+    ,
+    {
+        keyCode: 65,
+        keyTrigger: 'A',
+        id: 'Chord-4',
+        url: './sounds/chord-4.mp3'
+    }
+    ,
+    {
+        keyCode: 83,
+        keyTrigger: 'S',
+        id: 'HiHat-1',
+        url: './sounds/hihat-1.wav'
+    }
+    ,
+    {
+        keyCode: 68,
+        keyTrigger: 'D',
+        id: 'HiHat-2',
+        url: './sounds/hihat-2.wav'
+    }
+    ,
+    {
+        keyCode: 90,
+        keyTrigger: 'Z',
+        id: "Kick",
+        url: './sounds/kick-1.wav'
+      },
+      {
+        keyCode: 88,
+        keyTrigger: 'X',
+        id: 'Snare-1',
+        url: './sounds/snare-1.wav'
+      },
+      {
+        keyCode: 67,
+        keyTrigger: 'C',
+        id: 'Snare-2',
+        url: './sounds/snare-2.wav'
+      }
 ]
 function App(){
     return (
        <div className="wrapper">
-           <h2>LO-FI Drum Machine</h2>
-           {audioClips.map(clip => 
+           <h2>L O - F I</h2>
+           {audioClips.map((clip) => 
             <Pad clip={clip} key={clip.id}/>
             )}
        </div>
@@ -18,8 +74,22 @@ function App(){
 }
 
 function Pad({clip}) {
+
+  React.useEffect(() => {
+      document.addEventListener('keydown',handleKeyPress) 
+      return(()=>{
+          document.removeEventListener('keydown',handleKeyPress)
+      })
+  })
+
+  const handleKeyPress = (e) => {
+   if(e.keyCode == clip.keyCode) {
+       playSound();
+   }
+  }
+
   const playSound = () => {
-  const audioTag = document.getElementById('clip.keyTrigger');
+  const audioTag = document.getElementById(clip.keyTrigger);
   audioTag.currentTime = 0;
   audioTag.play();
     }
